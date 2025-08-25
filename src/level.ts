c27c79caa6ba6e28c1ea55b3b1697c5cb899118a
AJ Ruf <z0isch@gmail.com>
2025-08-25 11:24:47 -0400

    Countdown

--- src/level.ts
+++ src/level.ts
@@ -1,4 +1,13 @@
-import { Engine, Entity, Scene } from "excalibur";
+import {
+  Actor,
+  Color,
+  Engine,
+  Entity,
+  Font,
+  Scene,
+  Text,
+  Vector,
+} from "excalibur";
 import { Resources } from "./resources";
 import { MetronomeComponent, MetronomeSystem } from "./metronome";
 import { Player } from "./player";
@@ -29,9 +38,27 @@ export class MyLevel extends Scene {
     };
     this.add(clicktrack);
 
-    // Trigger to start all metronome things
-    engine.clock.schedule(() => {
-      metronomeSystem.trigger();
-    }, 300);
+    const countdown = new Actor({ pos: new Vector(400, 300) });
+    countdown.onInitialize = () => {
+      let text = new Text({
+        text: "3",
+        font: new Font({ size: 100 }),
+        color: Color.White,
+      });
+      countdown.graphics.add("countdown", text);
+      engine.clock.schedule(() => {
+        text.text = "2";
+      }, 1000);
+      engine.clock.schedule(() => {
+        text.text = "1";
+      }, 2000);
+      engine.clock.schedule(() => {
+        countdown.graphics.remove("countdown");
+        metronomeSystem.trigger();
+      }, 3000);
+      countdown.graphics.use("countdown");
+    };
+
+    this.add(countdown);
   }
 }
