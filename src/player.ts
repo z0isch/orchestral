492e2a4c3d202395eb18d8ee07d610df118e9e7f
AJ Ruf <z0isch@gmail.com>
2025-08-26 10:41:38 -0400

    Fix lineend

--- src/player.ts
+++ src/player.ts
@@ -86,7 +86,7 @@ export class Player extends Actor {
   override onPreUpdate(engine: Engine, elapsedMs: number): void {
     const mousePos =
       engine.input.pointers.currentFramePointerCoords.get(0)?.worldPos;
-    let lineEnd = mousePos?.sub(this.pos);
+
     const frameBeat = this.get(MetronomeComponent).frameBeat;
     if (frameBeat !== null && mousePos) {
       const direction = mousePos.sub(this.pos);
@@ -96,25 +96,21 @@ export class Player extends Actor {
       if (frameBeat % 4 === 2) {
         offset = offset.scale(-1);
       }
-      lineEnd = offset.add(vec(31, 31));
       this.actions.moveBy({
         offset: offset,
         duration: 200,
       });
     }
-
-    if (lineEnd) {
-      const line = new Line({
-        start: vec(15, 15),
-        end: lineEnd,
-        color: Color.White,
-        thickness: 4,
-      });
-      line.opacity = 0.1;
-      this._lineActor.graphics.use(line, {
-        anchor: vec(0, 0),
-        offset: vec(-15, -15),
-      });
-    }
+    const line = new Line({
+      start: vec(15, 15),
+      end: mousePos?.sub(this.pos).add(vec(15, 15)) ?? vec(15, 15),
+      color: Color.White,
+      thickness: 4,
+    });
+    line.opacity = 0.1;
+    this._lineActor.graphics.use(line, {
+      anchor: vec(0, 0),
+      offset: vec(-15, -15),
+    });
   }
 }
